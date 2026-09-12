9593194686bf535772c3a7acb4391cdf82c2b05f
rich-shaoyao <rich-shaoyao@users.noreply.github.com>
2026-09-12 12:57:55 +0800

    Initial commit: find_words SwiftUI app

---
+++ find_words/find_wordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  find_wordsApp.swift
+//  find_words
+//
+//  Created by 李少尧 on 2026/9/12.
+//
+
+import SwiftUI
+
+@main
+struct find_wordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
